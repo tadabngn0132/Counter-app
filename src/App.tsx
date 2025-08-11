b81c453ef87729b6576ecfc24654c1a9053c1380
Nguyen Ba Dat <datngba2310@gmail.com>
2025-08-11 11:23:08 +0700

    implemented core features which contain display current counter value, increase counter value, decrease counter value and reset counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -8,26 +6,16 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h1 className='mb-15 text-cyan-300 text-4xl'>Counter app</h1>
+      <div className='flex flex-col items-center gap-6'>
+        <div className='flex gap-2'>
+          <button onClick={() => {setCount(count => count - 1)}}>-</button>
+          <button>{count}</button>
+          <button onClick={() => {setCount(count => count + 1)}}>+</button>
+        </div>
+
+        <button onClick={() => {setCount(0)}}>Reset</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
